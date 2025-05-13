8040cb0255224063650c1cdb530ccf04ee8cbe53
Ravi Jangra <159130801+RJ-acc@users.noreply.github.com>
2025-05-13 13:07:51 +0530

    Update main.tf

--- AWS/IAC/main.tf
+++ AWS/IAC/main.tf
@@ -1,9 +1,7 @@
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
-data "aws_ecrpublic_authorization_token" "token" {
-  provider = aws.ecr
-}
+
 data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
@@ -12,11 +10,6 @@ data "aws_iam_session_context" "current" {
 }
 
 provider "aws" {
-  region = var.aws_region
-}
-
-provider "aws" {
-  alias  = "ecr"
   region = var.aws_region
 }
 
